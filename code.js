3c568e2defcf0ba6af187de41281ecde12ba62d6
adammuflihun <adambagusm@gmail.com>
2025-02-26 17:15:11 +0700

    Initial commit

---
+++ code.js
@@ -0,0 +1,9 @@
+gsap.to(".element", {
+
+    duration: 1,
+    x: 100, // 100px                
+    y: 200, // 200px
+    rotation: 360, // 360 degrees
+    scale: 2, // 2x
+    ease: "power2.inOut" // ease in and out
+});
